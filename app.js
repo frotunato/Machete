6fb3ce09480de0e3e795981c02c507e98232502d
frotunato <frotunato@gmail.com>
2016-08-18 01:36:12 +0100

    improved performance

--- app.js
+++ app.js
@@ -133,8 +133,8 @@ fragment.appendChild(parentDiv);
 var currentIframe;
 var currentId = '';
 var playerInstances = document.getElementsByClassName('video-container');
-*/
 var ta = Date.now();
+var posts = document.getElementById('posts');
 var currentIframe;
 var fragment = document.createDocumentFragment();
 var iframe = document.createElement('iframe');
@@ -146,17 +146,117 @@ iframe.height = '315';
 iframe.frameBorder = '0';
 iframe.allowFullScreen = "true";
 fragment.appendChild(iframe);
+*/
 
+/*
+var elements = document.getElementsByTagName('SCRIPT');
+console.log(elements[0].length)
+for (var i = elements.length - 1; i >= 0; i--) {
+  
+  if (elements[i].tagName === 'SCRIPT') {
+    console.log(elements[i])
+  }
+}
+*/
+//if (!posts) {
+//  var stopInterval = function () {
+//    clearInterval(interval);
+//    var scripts = document.getElementsByTagName( 'script' );
+//    var thisScriptTag = scripts[ scripts.length - 1 ];
+//    console.log(thisScriptTag);
+//    observer.observe(posts, {attributes: true, subtree:true, childList: true, characterData: true});
+//  };
+//  var interval = setInterval(function () {
+//    console.log(document.body)
+//    posts = document.body;
+//    if (posts) {
+//      stopInterval();
+//    }
+//  }, 5);
+//}
+
+
+
+
+var scripts;
+var tA = Date.now();
+function verVideo (id, frameId) {
+  var fragment = document.createDocumentFragment();
+  var parentDiv = document.createElement('div');
+  var container = document.getElementById(frameId);
+  var childDiv = document.createElement('div');
+  var iframe = document.createElement('iframe');
+  parentDiv.align = 'center';
+  parentDiv.className = 'video-youtube';
+  childDiv.className = 'video-container';
+  iframe.title = 'YouTube video player';
+  iframe.className = 'youtube-player';
+  iframe.type = 'text/html';
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.frameBorder = '0';
+  iframe.allowFullScreen = "true";
+  childDiv.appendChild(iframe);
+  parentDiv.appendChild(childDiv);
+  fragment.appendChild(parentDiv);
+  iframe.src = 'https://www.youtube.com/embed/' + id;
+  container.parentNode.replaceChild(fragment, container)
+}
+
+var observer = new MutationObserver(function (mutations) {
+  if (!document.body) return;
+  for (var i = mutations.length - 1; i >= 0; i--) {
+    for (var j = mutations[i].addedNodes.length - 1; j >= 0; j--) {
+      if (mutations[i].addedNodes[j].nodeName === 'SCRIPT' && (mutations[i].addedNodes[j].getAttribute('language') === 'javascript' || mutations[i].addedNodes[j].textContent.indexOf('verVideo') > 0)) {
+        //console.log(mutations[i].addedNodes[j].getAttribute('language'))
+        observer.disconnect();
+        scripts = mutations[i].addedNodes[j];
+        scripts.parentNode.removeChild(scripts) 
+        var newScript = document.createElement('script');
+        var textNode = document.createTextNode(verVideo.toString());
+        newScript.appendChild(textNode);
+        newScript.type = 'text/javascript';
+        document.head.appendChild(newScript);
+        console.log(Date.now() - tA, i, j);
+        break;
+      }
+    }
+  }
+  //scripts = document.getElementsByTagName('script');
+  //scripts = scripts[scripts.length - 1];
+  //if (scripts.getAttribute('language') === 'javascript') {
+  //  scripts.parentNode.removeChild(scripts) 
+  //  var newScript = document.createElement('script');
+  //  var textNode = document.createTextNode(verVideo.toString());
+  //  newScript.appendChild(textNode);
+  //  newScript.type = 'text/javascript';
+  //  document.head.appendChild(newScript);
+  //  console.log(Date.now() - tA);
+  //  observer.disconnect();
+  //}
+
+ 
+})
+
+observer.observe(document, {subtree:true, childList: true});
+
+
+
+/*
 document.addEventListener("DOMNodeInserted", function (event) {
-  if (event.target.className === 'video-youtube') {
+  console.log(event.target.tagName === 'video-youtube')
+  if (event.target.tagName === 'video-youtube') {
+    e.preventDefault();
+    console.log(event.target.nodeName)
+   
    // ta = Date.now();
-    currentIframe = event.target.firstChild.firstChild;
+    currentIframe = event.target;
     iframe.src = '//www.youtube.com/embed/' + currentIframe.src.split("/")[6];
     currentIframe.parentNode.replaceChild(fragment.cloneNode(true), currentIframe);
    // console.log('delta', Date.now() - ta)
-  } else if (event.target.tagName === 'SCRIPT') {
+  } else if (event.target.nodeName === 'SCRIPT') {
      event.target.parentNode.removeChild(event.target);
    //  console.log(event.target.parentNode)
   }
   //insertedNodes.push(e.target);
-}, false);
+}, false);*/
